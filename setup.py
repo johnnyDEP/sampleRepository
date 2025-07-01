d68a00e3450b954b3a1cb408f08f341018fcb4d5
johnnyDEP <johnnyDEP@users.noreply.github.com>
2025-07-01 16:52:29 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="sample-python-project",
+    version="0.1.0",
+    description="A sample Python project for doppleGanker testing.",
+    install_requires=[
+        "requests>=2.31.0",
+        "flask>=2.0.0",
+        "numpy"
+    ],
+) 
